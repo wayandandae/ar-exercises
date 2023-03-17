f496b5b8c8fb15f0b042106e0e820ba3a1064cfe
Jack Kang <jkang0724@hotmail.com>
2023-03-17 00:56:58 -0400

    Exercise 5: Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total Revenue for All Stores: #{Store.sum(:annual_revenue)}"
+puts "Average Revenue for All Stores: #{Store.average(:annual_revenue)}"
+# Combine where and size to perform calculation on selected query
+puts "Number of Stores with $1M> Revenue: #{Store.where("annual_revenue >= 1000000").size}"
