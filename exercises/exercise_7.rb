f7a0441395a87b641fc31189b0326794c07ae34b
Jack Kang <jkang0724@hotmail.com>
2023-03-17 01:19:44 -0400

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# Prompt user to enter a store name
+puts "Enter a store name: "
+user_store = gets.chomp
+# Create new store using the user input
+store = Store.new(name: user_store)
+# Save the store
+store.save
+
+# For each error message created by the store creation
+store.errors.full_messages.each do |errormsg|
+  # Print error message in each line
+  puts errormsg
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_apparels
+
+  def must_carry_apparels
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "can't be blank at the same time with women's")
+      errors.add(:womens_apparel, "can't be blank at the same time with men's")
+    end
+  end
 end
